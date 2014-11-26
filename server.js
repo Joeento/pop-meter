b1607b19e9d75a3a1a3976949ed587084a4b6914
Eric Kudler <joeento@gmail.com>
2014-11-26 01:43:13 -0500

    All FB api calls and algorithm are now in place

--- server.js
+++ server.js
@@ -30,7 +30,55 @@ app.get('/results', function(req, res) {
         	return;
 		}
         FB.setAccessToken(response.access_token);
-        res.render('pages/results', {access_token: response.access_token});
+
+        /* Algorithm begins here */
+        var max_friends = 600;
+        var three_weeks_ago = new Date();
+        three_weeks_ago.setDate(three_weeks_ago.getDate()-21);
+        console.log(three_weeks_ago.getTime());
+        var max_photos = 5;
+        var max_stream = 5;
+        var max_likes = 15;
+        FB.api('me/friends', {limit: max_friends}, function (friends) {
+            if(!friends || friends.error) {
+                console.log(!friends ? 'error occurred' : friends.error);
+                return;
+            }
+            FB.api('me/photos', {since: three_weeks_ago.getTime()}, function (photos) {
+                if(!photos || photos.error) {
+                    console.log(!photos ? 'error occurred' : photos.error);
+                    return;
+                }
+                FB.api('me/statuses', {limit: max_stream}, function (stream) {
+                    if(!stream || stream.error) {
+                        console.log(!stream ? 'error occurred' : stream.error);
+                        return;
+                    }
+
+                    var likes = 0;
+                    var photo_likes = 0;
+                    for (var i = 0; i< stream.data.length;i++) {
+                        likes += stream.data[i].likes.data.length;
+                    }
+                    var total_friends = Math.min(friends.data.length, max_friends);
+                    var total_photos = Math.min(photos.data.length,max_photos);
+                    var total_likes = Math.min(likes, max_likes);
+                    var pop = ((total_friends/max_friends)*20)
+                        + ((total_photos/max_photos)*10)
+                        + ((total_likes/max_likes)*10)
+                        + 60;
+                    console.log("(("  +total_friends+"/"+max_friends+")*20)" 
+                        + "+ ((" + total_photos+ "/"+max_photos+")*10)"
+                        +" + ((" + total_likes + "/"+max_likes+"*10) + 60;");
+                    res.render('pages/results', {pop: parseInt(pop)});
+                });
+            });
+
+        });
+
+        /* Algorithm ends here */
+
+       
 	});
     //Log an error?
 	
