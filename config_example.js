45be488d320788920145a3589d7c12356e596779
Eric Kudler <joeento@gmail.com>
2014-12-06 12:33:52 -0500

    logs will now default to folder inside directory, this can be cchanged in config file

--- config_example.js
+++ config_example.js
@@ -2,5 +2,8 @@ module.exports = {
 	app_id: 'YOUR_ID_HERE',
 	app_secret: 'YOUR_SECRET_HERE'
 	port: 'YOUR_PORT HEREs'
-	error_log: 'YOUR_LOG_DIR_HERE'
+	logs: {
+		info: 'logs/info.log',
+		error: 'logs/error.log',
+	}
 }
