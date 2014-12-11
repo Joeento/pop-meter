b28e48b37b2392e31bc168e283e59d25c89dc175
Eric Kudler <joeento@gmail.com>
2014-12-11 15:26:04 -0500

    JSHinted server file

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+/*jslint node: true */
+'use strict';
+
 var express = require('express');
 var url = require('url');
 var FB = require('fb');
@@ -25,18 +28,18 @@ var log = bunyan.createLogger({
 
 
 app.get('/', function(req, res) {
-    log.info('index')
+    log.info('index');
 	res.render('pages/index');
 });
 
 app.get('/auth', function(req, res) {
 	var redirect_uri = encodeURIComponent(req.protocol + '://' + req.headers.host + '/results');
-    log.info('auth')
+    log.info('auth');
 	res.redirect('https://www.facebook.com/dialog/oauth?client_id=' + config.app_id + '&redirect_uri=' + redirect_uri + '&scope=publish_stream,read_stream,user_photos,friends_photos');
 });
 
 app.get('/results', function(req, res) {
-    log.info('results')
+    log.info('results');
     var url_parts = url.parse(req.url, true);
 	FB.api('oauth/access_token', {
     	client_id: config.app_id,
@@ -44,7 +47,7 @@ app.get('/results', function(req, res) {
     	redirect_uri: req.protocol + '://' + req.headers.host + '/results',
     	code: url_parts.query.code
 	}, function (response) {
-        log.info({code: url_parts.query.code,response: response},'access_token')
+        log.info({code: url_parts.query.code,response: response},'access_token');
     	if(!response || response.error) {
             log.error({level: 'response', error: response.error});
         	return;
@@ -75,8 +78,7 @@ app.get('/results', function(req, res) {
                     }
 
                     var likes = 0;
-                    var photo_likes = 0;
-                    for (var i = 0; i< stream.data.length;i++) {
+                    for (var i = 0; i<stream.data.length;i++) {
                         likes += stream.data[i].likes.data.length;
                     }
                     var total_friends = Math.min(friends.data.length, max_friends);
@@ -90,23 +92,23 @@ app.get('/results', function(req, res) {
 
                     var byline = 'YOU ARE WELL KNOWN';
                     var image = 70;
-                    var desc = 'You are recognizable around your community.  However, if you are not happy, we reccomend introducing yourself to more people, maybe takign some pictures when you go out with friends.'
+                    var desc = 'You are recognizable around your community.  However, if you are not happy, we reccomend introducing yourself to more people, maybe takign some pictures when you go out with friends.';
                     if (pop >= 70 && pop < 80) {
-                        byline = 'YOU ARE FAIRLY POPULAR'
+                        byline = 'YOU ARE FAIRLY POPULAR';
                         image = 70;
-                        desc = 'Nice job!  You have reached a level of popularity in life you should be proud of.  However, if you are not happy, we reccomend introducing yourself to more people, maybe takign some pictures when you go out with friends.'
+                        desc = 'Nice job!  You have reached a level of popularity in life you should be proud of.  However, if you are not happy, we reccomend introducing yourself to more people, maybe takign some pictures when you go out with friends.';
                     } else if (pop >= 80 && pop < 90) {
-                        byline = 'YOU ARE PRETTY POPULAR'
+                        byline = 'YOU ARE PRETTY POPULAR';
                         image = 80;
                         desc = 'People must look up to you because your popularity is impressive.  The only thing that could make you any better is if you introduced yourself to more people and networked, but you\'re doing pretty well on your own.';
                     } else if (pop >= 90 && pop < 80) {
-                        byline = 'YOU ARE REALLY POPULAR!!!'
+                        byline = 'YOU ARE REALLY POPULAR!!!';
                         image = 90;
                         desc = 'Wow!  You are incredibly popular, you have lots of friends and are constantly being included.  Congratulations!!!';
                     }
-                    log.info({pop: pop, math: "(("  +total_friends+"/"+max_friends+")*20)" 
-                        + "+ ((" + total_photos+ "/"+max_photos+")*10)"
-                        +" + ((" + total_likes + "/"+max_likes+"*10) + 60;"}, 'success');
+                    log.info({pop: pop, math: '(('  +total_friends+'/'+max_friends+')*20)' 
+                        + '+ ((' + total_photos+ '/'+max_photos+')*10)'
+                        +' + ((' + total_likes + '/'+max_likes+'*10) + 60;'}, 'success');
 
                     res.render('pages/results', {pop: pop, byline: byline, image: image, desc: desc, app_id: config.app_id});
                 });
